88630da797e7fada09012a7c05878f49e5d5ee67
JEONGWon9 <JEONGWon9@users.noreply.github.com>
2021-10-21 23:21:40 +0900

    수습?

--- Project_BC/src/main/java/controller/AdminController.java
+++ Project_BC/src/main/java/controller/AdminController.java
@@ -38,17 +38,11 @@ public class AdminController extends HttpServlet {
 			forward = new ActionForward();
 			forward.setPath("/adminPage/sellerInfo.jsp");
 			forward.setRedirect(false); // Dispatcher 방식(기본값이므로 생략 가능)
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'master' of https://github.com/ITWILL1TEAM/Banchan.git
 		}else if(command.equals("/side.ad")) {			
 			forward = new ActionForward();
 			forward.setPath("/category/sidedish.jsp");
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'master' of https://github.com/ITWILL1TEAM/Banchan.git
 		}else if(command.equals("/ProductList.ad")) {			
 			forward = new ActionForward();
 			forward.setPath("/adminPage/productList.jsp");
@@ -56,10 +50,6 @@ public class AdminController extends HttpServlet {
 		}else if(command.equals("/Notice.ad")) {			
 			forward = new ActionForward();
 			forward.setPath("/adminPage/Notice.jsp");
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'master' of https://github.com/ITWILL1TEAM/Banchan.git
 			forward.setRedirect(false); // Dispatcher 방식(기본값이므로 생략 가능)
 		}else if(command.equals("/ProductRegister.ad")) {			
 			forward = new ActionForward();
