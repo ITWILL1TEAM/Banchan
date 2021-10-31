c247b267dd905649e21baf01401c416353c46b7b
Jaehee Bae <gjznn3657@gmail.com>
2021-10-31 23:37:29 +0900

    충돌 ㅠㅠ

--- Project_BC/src/main/java/action/ProductDetailAction.java
+++ Project_BC/src/main/java/action/ProductDetailAction.java
@@ -7,9 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import svc.ProductDetailService;
 import svc.ReviewListService;
-import vo.ActionForward;
-import vo.BoardBean;
-import vo.ReviewBean;
+import vo.*;
 
 public class ProductDetailAction implements Action {
 
@@ -20,19 +18,24 @@ public class ProductDetailAction implements Action {
 		ActionForward forward = null;
 		
 		// request 객체를 통해 전달받은 파라미터(product_num) 가져오기
-		int product_num = 2;
+		int product_num = Integer.parseInt(request.getParameter("product_num"));
+	
 		
+		System.out.println("detail"+product_num);
 		// BoardDetailService 클래스 인스턴스 생성 후 
+
 		ProductDetailService service = new ProductDetailService();
 		
 		// getArticle() 메소드 호출하여 board_num에 대한 게시물 상세 정보 리턴받기
 		// -> 파라미터 : 글번호(board_num)  리턴타입 : Boardbean(article)
 		BoardBean article = service.getArticle(product_num);
+		ProductImg productImg= service.getArticleImg(product_num);
 //		int listCount = review.getListCount();
 //		ArrayList<ReviewBean> reviewList = service.getReviewList(product_num, page, limit);
 		
 		// 다음 페이지로 전달하기 위한 객체를(article, page) request 객체에 저장
 		request.setAttribute("article", article);
+		request.setAttribute("productImg", productImg);
 		
 		// ActionForward 객체를 생성하여 /product/productDetail.jsp 페이지로 포워딩
 		// -> request 객체를 유지해야하므로 Dispatcher 방식으로 포워딩
